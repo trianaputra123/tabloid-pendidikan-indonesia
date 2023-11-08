5eb4a25de7060083d772a9d5b3db28fa13310b76
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2023-11-08 11:07:15 +0800

    copyright

--- resources/views/components/other/footer.blade.php
+++ resources/views/components/other/footer.blade.php
@@ -88,7 +88,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
         © 2020 Copyright:
-        <a class="text-white" href="#"> Gede Yuda Aditya</a>
+        <a class="text-white" href="#"> Tabloid Pendidikan Indonesia</a>
     </div>
     <!-- Copyright -->
 </footer>
